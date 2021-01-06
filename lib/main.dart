b81ff8efc367471e695d99f30616844f872d6541
Victor Pereira <vitutiv@localhost.localdomain>
2021-01-06 20:05:42 -0300

    1.0 - First commit

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'NiceDice',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: DicePage(),
+    );
+  }
+}
+
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text('Nice Dice'),
+      ),
+      body: Center(
+        child: Row(
+          children: [
+            Expanded(
+              child: Dice(),
+            ),
+            Expanded(
+              child: Dice(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Dice extends StatefulWidget {
+  @override
+  _DiceState createState() => _DiceState();
+}
+
+class _DiceState extends State<Dice> {
+  int diceNumber = rollDice();
+
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      onPressed: () {
+        setState(() {
+          diceNumber = rollDice();
+          print('Dice number = $diceNumber');
+        });
+      },
+      child: Image.asset('assets/images/dice$diceNumber.png'),
+    );
+  }
+
+  static int rollDice() {
+    int diceNumber = Random().nextInt(6) + 1;
+    return diceNumber;
+  }
+}
